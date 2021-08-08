cd958ceb83f5464ac278887986434c7b7a24e643
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-08-08 03:15:26 +0300

    solution

--- app.js
+++ app.js
@@ -108,84 +108,72 @@ console.log('Hello from the new object function', newObject(['hi', 'hello', 'are
 
 
 
-let sum = function(a, b, c, d) {
-  return a + b + c + d;
-};
+let sum =(a, b, c, d)=> a + b + c + d;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sum(1, 2, 3, 4));
+console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
-  return {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-  };
-};
-
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(objectLit());
-
-
-let sumAndProduct = function(a, b) {
-  let sum = a + b;
-  let product = a * b;
-  return [sum, product];
-};
+let objectLit = ()=> ({
+  
+    key1 : 'value1',
+    key2 : 'value2',
+    key3 : 'value3'
+  
+});
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
+console.log(objectLit());
 
 
-let message = function(name) {
-  return `Hello, ${name}!`;
-};
+let sumAndProduct =(a, b)=> ({
+   sum :a + b,
+   product : a * b
+  // [sum, product],
+});
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
+console.log(sumAndProduct(3, 9));
 
 
-let Student = function(name, age, hometown) {
-  this.name = name;
-  this.age = age;
-  this.hometown = hometown;
-};
+let message = (name)=>`Hello, ${name}!`;
 
-let joe = new Student('Joe', 'Schmoe', 100);
+// TODO: Uncomment the following line of code to see the output in the browser console
+console.log(message('Allie'));
+
+
+let Student = (name, age, hometown)=> `${name} ${age} ${hometown}`;
+
+let joe = Student('Joe', 'Schmoe', 100);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
 
 
-Student.prototype.greeting = function() {
-  return `Hi, my name is ${this.name}`;
-};
+Student.prototype.greeting = ()=> `Hi, my name is ${this.name}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
 
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+Student.courseName = ()=> 'This student is enrolled in Code 301.';
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
 // STEP 11
 // How do arrow functions affect constructor functions?
-Student.prototype.scope = function() {
+Student.prototype.scope =()=> {
   console.log(this);
 };
 
